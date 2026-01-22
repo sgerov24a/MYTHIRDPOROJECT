c2527f91f7e7cec3e169c045ed2470ad4f8f0cd3
sgerov24a <sgerov24a@gmail.com>
2026-01-22 10:36:39 +0200

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 import Streamlit as st
+st.title("LoginSystem")
+name  = st.text_input("Enter name:")
+if st.button("Check"):
+if name strip()=="":
+st.warniong("Please enter text.")
+elif not name is apha():
+st.warning("")
+else st.success("The text is corect!")
